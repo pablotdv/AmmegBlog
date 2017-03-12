7698a60fd707547eba923989c9be14872937bc71
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-03-12 19:42:07 -0300

    Modelagem da entidade Post

--- Ammeg.Blog/Ammeg.Blog/Models/Post.cs
+++ Ammeg.Blog/Ammeg.Blog/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,12 +10,16 @@ namespace Ammeg.Blog.Models
     [Table("Posts")]
     public class Post
     {
+        [Key]
         public Guid PostId { get; set; }
 
         public string Titulo { get; set; }
 
         public string Conteudo { get; set; }
 
+        public Guid AutorId { get; set; }
 
+        [ForeignKey(nameof(AutorId))]
+        public virtual ApplicationUser Usuario {get;set;}
     }
 }
--- Ammeg.Blog/Ammeg.Blog/Startup.cs
+++ Ammeg.Blog/Ammeg.Blog/Startup.cs
@@ -88,6 +88,13 @@ namespace Ammeg.Blog
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+                context.Database.Migrate();
+                context.EnsureSeedData();
+            }
         }
     }
 }
